419533402057814b22a954128ae516a92a25f5f3
bhd433 <oskari.aho@myy.haaga-helia.fi>
2024-05-09 21:06:00 +0300

    tt

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import './App.css'; // Import CSS file for styling
+import './App.css';
 
 
 // jsx importit
